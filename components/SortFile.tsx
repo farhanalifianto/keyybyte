de86111332f27672023fc07d1dfdc8f3678e3c6e
farhanalifianto <farhanalifianto@gmail.com>
2024-12-18 23:01:40 +0700

    feat:sort files

--- components/SortFile.tsx
+++ components/SortFile.tsx
@@ -1,7 +1,38 @@
+"use client";
+
 import React from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { sortTypes } from "@/constant";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const path = usePathname();
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+  return (
+    <>
+      <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder={sortTypes[0].value} />
+        </SelectTrigger>
+        <SelectContent>
+          {sortTypes.map((type) => (
+            <SelectItem key={type.value} value={type.value}>
+              {type.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </>
+  );
 };
 
 export default Sort;
